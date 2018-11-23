2126e7be18a09804270aa9f09e5c0239dbe123df
milomndz <00050515@uca.edu.sv>
2018-11-23 07:18:35 -0600

    consultar

--- _investigador/index.php
+++ _investigador/index.php
@@ -88,7 +88,7 @@
               <p class="card-text mb-auto">ISBN: <?php echo $row['isbn']. "<br>";
                 echo "Estanteria: ".$row['estanteria'];  ?>
               </p>
-              <a href="#">Leer más</a>
+              <button type="button" class="btn btn-info btn-large" data-toggle="modal" data-target="#exampleModal" data-whatever="<?php echo $row['isbn'];  ?>" data-titulo="<?php echo $row['titulo'];  ?>">Consultar</button>
             </div>
             <img class="card-img-right flex-auto d-none d-lg-block"  src="../img/portada2.jpg" width="200px" weight="200px" alt="Card image cap">
           </div>
@@ -106,7 +106,40 @@
         <a href="#">Back to top</a>
       </p>
     </footer>
-
+    
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title" id="exampleModalLabel">New message</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form action="../dao/prestar.php" method="POST">
+              <div class="form-group">
+                <label for="recipient-name" class="col-form-label">ISBN:</label>
+                <input type="text" disabled class="form-control" id="recipient-name" name="recipient-name">
+              </div>
+              <div class="form-group">
+                <label for="message-text" class="col-form-label">Titulo:</label>
+                <input type="text" id="titulo-name" name="titulo-name" disabled class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="message-text" class="col-form-label">Token:</label>
+                <input type="text" id="token-name" name="token-name" class="form-control">
+              </div>
+            
+          </div>
+          <div class="modal-footer">
+            <button  class="btn btn-danger" id="btnPrestar"  type="submit">Prestar</button>
+          </div>
+          </form>
+        </div>
+      </div>
+    </div>	  
+	  
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
@@ -121,6 +154,19 @@
         fg: '#eceeef',
         text: 'Thumbnail'
       });
+    </script>
+    <script>
+      $('#exampleModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('whatever') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var tituloL = button.data('titulo')
+        var modal = $(this)
+        modal.find('.modal-title').text('Prestar libro: ' + tituloL)
+        modal.find('#recipient-name').val(recipient)
+        modal.find('#titulo-name').val(tituloL)
+      })
     </script>
   </body>
 </html>
